43622208e80639c7e644d52ef06fe7183c18ec4c
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-10-07 14:02:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+//import all the needed react-bootstrap components
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* Navigation and Header */}
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Logo</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <br/>
+
+      
+    {/* Card section */}
+    <Container className='d-flex flex-row'>
+      <Card style={{ width: '18rem', marginLeft: '20px' }}>
+        <Card.Body>
+          <Card.Title>Welcome Home</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Where is home?</Card.Subtitle>
+          <Card.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione expedita pariatur sint neque minus error molestias odit aliquam. Ut.
+          </Card.Text>
+          <Card.Link href="#">About us</Card.Link>
+          <Card.Link href="#">Contact Us</Card.Link>
+        </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem', marginLeft: '20px' }}>
+        <Card.Body>
+          <Card.Title>Welcome Home</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Where is home?</Card.Subtitle>
+          <Card.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione expedita pariatur sint neque minus error molestias odit aliquam. Ut.
+          </Card.Text>
+          <Card.Link href="#">About us</Card.Link>
+          <Card.Link href="#">Contact Us</Card.Link>
+        </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem', marginLeft: '20px' }}>
+        <Card.Body>
+          <Card.Title>Welcome Home</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Where is home?</Card.Subtitle>
+          <Card.Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione expedita pariatur sint neque minus error molestias odit aliquam. Ut.
+          </Card.Text>
+          <Card.Link href="#">About us</Card.Link>
+          <Card.Link href="#">Contact Us</Card.Link>
+        </Card.Body>
+    </Card>
+  </Container>
+    </>
   );
 }
 
